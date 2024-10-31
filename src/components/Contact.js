41ea7ad2e283baa74326d19debfab03c1bbf59db
m0hamedKhalidMG <115906020+m0hamedKhalidMG@users.noreply.github.com>
2024-10-31 18:58:45 +0300

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -90,21 +90,40 @@ const Contact = () => {
   return (
     <ContactContainer>
       <Title>تواصل معنا</Title>
-      <Form onSubmit={handleSubmit}>
-        <Input
-          type="email"
-          name="email"
-          placeholder="بريدك الإلكتروني"
-          value={email}
-          onChange={handleChange}
-          required
-        />
-        <ContactText>
-      +966 55 429 9346 للوصول إلى مركز الاتصال، نأمل منك الاتصال على   
-        </ContactText>
-        <CallButton href="tel:+966 55 429 9346">الاتصال</CallButton>
-      </Form>
-    </ContactContainer>
+      <ContactText dir="rtl">
+  <span style={{ display: 'flex', alignItems: 'center', marginBottom: '5px' }}>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="20"
+      height="20"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      style={{ marginLeft: '5px' }}
+    >
+      <path d="M6.62 10.79a15.91 15.91 0 006.59 6.59l2.2-2.2c.27-.27.67-.36 1.02-.24 1.12.37 2.33.57 3.57.57.55 0 1 .45 1 1V20c0 .55-.45 1-1 1C9.4 21 3 14.6 3 7c0-.55.45-1 1-1h3.03c.55 0 1 .45 1 1 0 1.25.2 2.45.57 3.57.11.35.02.74-.24 1.02l-2.2 2.2z"/>
+    </svg>
+    <span>للوصول إلى مركز الاتصال، نأمل منك الاتصال على +966 55 429 9346</span>
+  </span>
+  
+  <span style={{ display: 'flex', alignItems: 'center' }}>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="20"
+      height="20"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      style={{ marginLeft: '5px' }}
+    >
+      <path d="M20 4H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm-1 4.25l-7 4.5-7-4.5V6l7 4.5 7-4.5v2.25z"/>
+    </svg>
+    <span>أو مراسلتنا عبر البريد الإلكتروني على Athaarksa@gmail.com</span>
+  </span>
+</ContactText>
+
+
+
+
+        </ContactContainer>
   );
 };
 
