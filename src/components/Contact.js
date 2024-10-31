5f83e39763f07e8b9e4fa0c47485094c263cf05e
m0hamedKhalidMG <115906020+m0hamedKhalidMG@users.noreply.github.com>
2024-10-31 19:06:56 +0300

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -94,8 +94,8 @@ const Contact = () => {
   <span style={{ display: 'flex', alignItems: 'center', marginBottom: '5px' }}>
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="20"
-      height="20"
+      width="25"
+      height="25"
       viewBox="0 0 24 24"
       fill="currentColor"
       style={{ marginLeft: '5px' }}
@@ -108,8 +108,8 @@ const Contact = () => {
   <span style={{ display: 'flex', alignItems: 'center' }}>
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="20"
-      height="20"
+      width="25"
+      height="25"
       viewBox="0 0 24 24"
       fill="currentColor"
       style={{ marginLeft: '5px' }}
